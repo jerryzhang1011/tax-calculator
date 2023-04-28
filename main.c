d545e4cee5c2af948bcb9182e567dfdc3ac7db51
jerryzhang1011 <131218857+jerryzhang1011@users.noreply.github.com>
2023-04-28 17:33:38 -0600

    added program

---
+++ main.c
@@ -0,0 +1,6 @@
+#include "tax_cal.h"
+
+int main(void) {
+    io_driver ();
+    return 0;
+}
---
+++ tax_cal.h
@@ -0,0 +1,46 @@
+//Command List:
+//             ON for Ontario
+//             BC for British Columbia
+//             AB for Alberta
+//             MB for Manitoba
+//             NB for New Brunswick
+//             NL for Newfoundland and Labrador
+//             NS for Nova Scotia
+//             PE for Prince Edward Island
+//             QC for Quebec
+//             SK for Saskatchewan
+//             NT for Northwest Territories
+//             NU for Nunavut
+//             YT for Yukon
+//             EX to exit program
+void list_command();
+
+double cal_fed_tax(double income);
+
+double cal_ON_tax(double income);
+
+double cal_BC_tax(double income);
+
+double cal_AB_tax(double income);
+
+double cal_MB_tax(double income);
+
+double cal_NB_tax(double income);
+
+double cal_NL_tax(double income);
+
+double cal_NS_tax(double income);
+
+double cal_PE_tax(double income);
+
+double cal_QC_tax(double income);
+
+double cal_SK_tax(double income);
+
+double cal_NT_tax(double income);
+
+double cal_NU_tax(double income);
+
+double cal_YT_tax(double income);
+
+void io_driver ();
